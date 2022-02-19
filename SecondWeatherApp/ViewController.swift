04cb338e1a960ece67071cbb0230170b68440b42
Bektemirov Shahzod <ioshakh27@gmail.com>
2022-02-19 15:16:57 +0500

    commit

---
+++ SecondWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondWeatherApp
+//
+//  Created by Shakhzod Bektemirov on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
